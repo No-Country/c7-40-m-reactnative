4cd598b9d790b079ba2558ef69f519e532277eda
Jocelyn <miranda@Jocelyns-Air.fibertel.com.ar>
2022-10-14 14:57:07 -0300

    error expo

--- src/Redux/Reducers/DataUserReducer.js
+++ src/Redux/Reducers/DataUserReducer.js
@@ -9,6 +9,7 @@ const DataUserReducer = (state = inicialState, action) => {
     switch (action.type) {
         case LOGIN_INFO:
             return {
+                ...state,
                 users: action.payload
             }
 
